cbe7fdc4b31d3c0d90809c4042db3df1d1476463
amongothers <zhengwenwei.123@gmail.com>
2013-07-08 22:00:44 +0800

    change line number implementation for reading not on cnblogs

--- run_highlight.js
+++ run_highlight.js
@@ -2,8 +2,7 @@ var content = "<pre><code class=\"javascript\">var function={}\neval(function)</
 var content1 = "<pre><code>var function={}\neval(function)</code></pre>";
 var content2 = "<pre><code class=\"javascript showln\">var function={}\neval(function)</code></pre>";
 var test = '<pre><code>var function={console.log("hello")}\n</code></pre>\n<pre><code class="javascript ln_off">var function={};//test </code></pre>';
-var test1 = '<pre><code class="no-highlight">var function={console.log(eval())};//test </code></pre>;\n<pre><code class="javascript ln_on">var function={};//test </code></pre>';
-var hljs = require('highlight');
+var test1 = "<pre><code class='javascript ln_on'>var test = function={\n  \n};</code></pre>"
 var hljs = require('highlight');
 var hl = hljs.Highlight;
 var output;
@@ -16,4 +15,4 @@ var output;
 //output = hl(test, false, true);
 //console.log(output);
 output = hl(test1, false, true);
-console.log(output);
+//console.log(output);
